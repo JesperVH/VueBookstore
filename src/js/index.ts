98655b32f24a41adccc00135ae7c1ed727a22717
Jesper Voldby Hansen <jesp58c0@edu.easj.dk>
2020-04-30 11:26:50 +0200

    Start repository

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,66 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index";
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+interface IBook {
+    id: number;
+    Title: string;
+    Author: string;
+    price: number;
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
+let baseUri: string = "http://anbo-bookstorerest.azurewebsites.net/api/Books"
+new Vue({
+    el: "#app",
+    data:{
+        books: [],
+        bookForm: {id: 0, Title: "", Author: "", price: 0},
+        deleteId: 0,
+        deleteMessage: "",
+        addMessage: ""
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+    },
+    methods: {
+        showBooks(){
+            console.log("show books")
+            axios.get<IBook[]>(baseUri)
+                .then((response: AxiosResponse<IBook[]>)=>{
+                    console.log(response.data)
+                    this.books = response.data
+                })
+                .catch((error: AxiosError) =>{
+                    alert(error.message)
+                })
+            
+        },
+
+        addBook(){
+            axios.post<IBook>(baseUri, this.bookForm)
+                .then((response: AxiosResponse) =>{
+                    let message: string = "response " + response.status + " " + response.statusText
+                    this.addMessage = message
+                    this.getBooks()
+                })
+                .catch((error: AxiosError) =>{
+                    alert(error.message)
+                })
+        },
+
+        deleteBook(deleteId: number){
+            let uri: string = baseUri + "/" + deleteId
+            axios.delete<void>(uri)
+                .then((response: AxiosResponse<void>) =>{
+                    this.deleteMessage = response.status + " " + response.statusText
+                })
+                .catch((error: AxiosError) =>{
+                    alert(error.message)
+                })
+        }
+    }
+})
+
+
+
+
+
+
